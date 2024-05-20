df6d8c2b8506bb8fa68f3aedb0cd3c2ac9bc8d6c
Mohirbeck <mustaqimovmohir@gmail.com>
2024-05-20 14:28:50 +0500

    change banner text

--- pages/index.vue
+++ pages/index.vue
@@ -6,12 +6,29 @@
       class="banner-swiper"
       :pagination="{ clickable: true, dynamicBullets: true }"
     >
-      <swiper-slide v-for="banner in banners" :key="banner.id" class="relative aspect-[339/133] lg:aspect-[48/11] bg-center bg-cover bg-no-repeat rounded-[9px] lg:rounded-[13px]" :style="`background-image: url(${banner.image})`">
-        <img :src="banner.products_image" alt="products" class="w-[55%] lg:w-[45%] absolute right-[9px] lg:right-10 bottom-[11px] lg:bottom-[34px]" />
-        <img src="~/assets/images/certificates.png" alt="banner" class="absolute lg:left-1/2 top-[9px] right-[17px] lg:right-auto lg:top-5 lg:-translate-x-1/2 w-[100px] lg:w-auto" />
-        <img src="~/assets/images/logo.svg" alt="logo" class="absolute left-[17px] lg:left-[6.7%] top-3 lg:top-9 w-[110px] lg:w-[270px]" />
+      <swiper-slide
+        v-for="banner in banners"
+        :key="banner.id"
+        class="relative aspect-[339/133] lg:aspect-[48/11] bg-center bg-cover bg-no-repeat rounded-[9px] lg:rounded-[13px]"
+        :style="`background-image: url(${banner.image})`"
+      >
+        <img
+          :src="banner.products_image"
+          alt="products"
+          class="w-[55%] lg:w-[45%] absolute right-[9px] lg:right-10 bottom-[11px] lg:bottom-[34px]"
+        />
+        <img
+          src="~/assets/images/certificates.png"
+          alt="banner"
+          class="absolute lg:left-1/2 top-[9px] right-[17px] lg:right-auto lg:top-5 lg:-translate-x-1/2 w-[100px] lg:w-auto"
+        />
+        <img
+          src="~/assets/images/logo.svg"
+          alt="logo"
+          class="absolute left-[17px] lg:left-[6.7%] top-3 lg:top-9 w-[110px] lg:w-[270px]"
+        />
         <p
-          class="absolute left-[17px] lg:left-[6.7%] top-1/3 lg:top-[40%] max-w-[137px] lg:max-w-[340px] font-light text-xs lg:text-[21px] lg:leading-[31.18px]"
+          class="absolute left-[17px] lg:left-[6.7%] top-1/3 lg:top-[40%] max-w-[137px] lg:max-w-[340px] font-light text-xs lg:text-lg 3xl:text-[21px] lg:leading-[27px] 3xl:leading-[31.18px]"
         >
           {{ banner.title }}
         </p>
@@ -81,8 +98,12 @@
           </div>
         </div>
         <div class="mt-6 lg:mt-16 flex items-center space-x-6">
-          <btn :to="localePath(`/products/${products[0].id}`)">{{ $t("buy") }}</btn>
-          <btn :to="localePath(`/products/${products[0].id}`)" outline>{{ $t("more") }}</btn>
+          <btn :to="localePath(`/products/${products[0].id}`)">{{
+            $t("buy")
+          }}</btn>
+          <btn :to="localePath(`/products/${products[0].id}`)" outline>{{
+            $t("more")
+          }}</btn>
         </div>
       </div>
       <div class="lg:ml-10 w-full relative lg:w-auto">
@@ -241,8 +262,12 @@
           </div>
         </div>
         <div class="mt-6 lg:mt-16 flex items-center space-x-6">
-          <btn :to="localePath(`/products/${products[1].id}`)">{{ $t("buy") }}</btn>
-          <btn :to="localePath(`/products/${products[1].id}`)" outline>{{ $t("more") }}</btn>
+          <btn :to="localePath(`/products/${products[1].id}`)">{{
+            $t("buy")
+          }}</btn>
+          <btn :to="localePath(`/products/${products[1].id}`)" outline>{{
+            $t("more")
+          }}</btn>
         </div>
       </div>
     </div>
